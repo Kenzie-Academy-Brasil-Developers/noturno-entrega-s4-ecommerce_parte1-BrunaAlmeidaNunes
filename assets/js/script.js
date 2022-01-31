d5109807aae82e0ac63fd3a989429d1b6af0463f
Bruna Almeida Nunes da Conceicao <brunamamamu@gmail.com>
2022-01-31 18:05:32 -0300

    grade de produtos

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+/*function leiaMais(){
+	let pontos = document.getElementsById('pontos');
+	let maisTexto = document.getElementsById('mais');
+	let bntLeiaMais = document.getElementsById('bntLeiaMais')
+
+	if(pontos.style.display === 'none'){
+		pontos.style.display = 'inline';
+		maisTexto.style.display = 'none';
+		bntLeiaMais.innerHTML = 'Leia Mais';
+	}else{
+		pontos.style.display = 'none';
+		maisTexto.style.display = 'inline';
+		bntLeiaMais.innerHTML ='Leia Menos';
+	}
+}*/
